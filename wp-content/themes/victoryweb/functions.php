02261d3c72d5fd064b6ec83620cde7ba64e7428c
Jay Jones <jaypjonesofficial@gmail.com>
2019-12-29 00:29:32 -0600

    VIC-001: Inital theme and framework set up (#1)

--- wp-content/themes/victoryweb/functions.php
+++ wp-content/themes/victoryweb/functions.php
@@ -8,3 +8,7 @@
  * Description: Require all theme functions files.
  * Documentation: http://codex.wordpress.org/Functions_File_Explained
  */
+
+require_once('lib/util/helpers.php'); // Global helper functions
+require_once('lib/util/data.php');       // Manage theme assets
+require_once('lib/assets.php');       // Manage theme assets
