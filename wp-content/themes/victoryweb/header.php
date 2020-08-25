c499b93b0ce2d53859ad345e3ab4904325f63ffe
Jay Jones <jaypjonesofficial@gmail.com>
2020-08-24 19:52:19 -0500

    Removing meta tag

--- wp-content/themes/victoryweb/header.php
+++ wp-content/themes/victoryweb/header.php
@@ -16,7 +16,6 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Victory Lash and Bow</title>
-	<link rel="author" href="http://jayjcodes.com">
 
 	<?php wp_head(); ?>
 </head>
